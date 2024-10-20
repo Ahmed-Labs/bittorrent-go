9f2fd48b437c0b7b89fceee0f514e5fbad28f875
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-10-19 21:56:16 -0400

    add bencoded integers to decoder

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -12,11 +12,30 @@ import (
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
 var _ = json.Marshal
 
+func isBencodeInteger(s string) bool {
+	return rune(s[0]) == 'i' && rune(s[len(s)-1]) == 'e'
+}
+
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
+	if isBencodeInteger(bencodedString) {
+		encoded := bencodedString[1:len(bencodedString)-1]
+		
+		// Check for leading 0's
+		if len(encoded) > 1 && (encoded[0] == '0' || 
+			encoded[0] == '-' && encoded[1] == '0') {
+			return nil, fmt.Errorf("Invalid encoded bencode integer: trailing 0's")
+		}
+
+		decoded, err := strconv.Atoi(encoded)
+		if err != nil {
+			return "", err
+		}
+		return decoded, nil
+
+	} else if unicode.IsDigit(rune(bencodedString[0])) {
 		var firstColonIndex int
 
 		for i := 0; i < len(bencodedString); i++ {
